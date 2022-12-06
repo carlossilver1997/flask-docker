7a004ba3c2bba1e2df664783b0ca481bb0e71c6e
carlos_flores <carlosdavidfloressilva@gmail.com>
2022-12-06 00:28:47 -0600

    adding hello world to the app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+class Test(Resource):
+    def get(self):
+        return { "message": "hello world" }
+
+api.add_resource(Test, '/test')
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 virtualenv venv --python=python3.9
 source venv/bin/activate
 deactivate
+pip3 install -r requirements.txt
